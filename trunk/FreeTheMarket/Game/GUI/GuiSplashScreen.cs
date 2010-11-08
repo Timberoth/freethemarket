de762ae9a10079c96be5588e59522604182833b4
consciousgamer <consciousgamer@d449e50b-a46f-350e-31ae-83ceb01ef3aa>
2010-11-08 07:41:35 +0000

    Checking in TorqueX framework template.

---
+++ trunk/FreeTheMarket/Game/GUI/GuiSplashScreen.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using GarageGames.Torque.Platform;
+using GarageGames.Torque.Core;
+using GarageGames.Torque.Sim;
+using GarageGames.Torque.GUI;
+using GarageGames.Torque.MathUtil;
+
+
+
+namespace StarterGame2D
+{
+    class GuiSplashScreen : GUISplash, IGUIScreen
+    {
+        GUISplashStyle splashStyle = new GUISplashStyle();
+
+
+        public GuiSplashScreen()
+        {
+        }
+
+
+
+        public GuiSplashScreen(string ImagePath)
+        {
+            InitializeSplashScreen(ImagePath);
+        }
+
+
+
+        private void InitializeSplashScreen(string ImagePath)
+        {
+            splashStyle.FadeInSec = 2f; // black to image 2 seconds
+            splashStyle.FadeOutSec = 2f; // image to black 2 seconds
+            splashStyle.FadeWaitSec = 3f; // still image for 3 seconds
+            splashStyle.Bitmap = ImagePath; //@"data\images\SplashGG";
+
+
+            // set some info for this control
+            Name = "GuiSplashScreen";
+            Style = splashStyle;
+            OnFadeFinished = OnSplashFinished;
+        }
+
+
+
+        public void OnSplashFinished()
+        {
+            if (splashStyle.Bitmap.EndsWith("splashStudio")) //splashStudio.png
+                Game.Instance.OnFinishedLoadSplashStudio();
+            else
+                Game.Instance.OnFinishedLoadSplashGarageGames();
+        }
+
+    }
+}
---
+++ trunk/FreeTheMarket/Game/Game.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using GarageGames.Torque.Core;
+using GarageGames.Torque.SceneGraph;
+using GarageGames.Torque.Sim;
+using GarageGames.Torque.GameUtil;
+using GarageGames.Torque.T2D;
+using GarageGames.Torque.GUI;
+using GarageGames.Torque.Platform;
+
+
+
+namespace StarterGame2D
+{
+    /// <summary>
+    /// This is the main game class for your game
+    /// </summary>
+    public class Game : TorqueGame
+    {
+        #region Static methods, fields, constructors
+        /// <summary>
+        /// A static property that lets you easily get the Game instance from any Game class.
+        /// </summary>
+
+        public static Game Instance { get { return _myGame; } }
+        public int Player1ControllerIndex { get; set; }
+        public int PlayerScore { get; set; }
+        public int HealthLevel { get; set; }
+
+        #endregion
+
+
+        #region Constructors
+        #endregion
+
+
+        #region Public properties, operators, constants, and enums
+        #endregion
+
+
+        #region Public Methods
+        public static void Main()
+        {
+            // Create the static game instance.  
+            _myGame = new Game();
+
+            // begin the game.  Further setup is done in BeginRun()
+            _myGame.Run();
+        }
+
+
+
+        public void OnFinishedLoadSplashStudio()
+        {
+            //next show the studio splash screen
+            GuiSplashScreen splashScreen = new GuiSplashScreen(@"data\images\TX_BG");
+            GUICanvas.Instance.SetContentControl(splashScreen);
+        }
+
+
+
+        public void OnFinishedLoadSplashGarageGames()
+        {
+            //load the main menu
+            GuiStartScreen startScreen = new GuiStartScreen();
+            GUICanvas.Instance.SetContentControl(startScreen);
+        }
+
+        #endregion
+
+
+        #region Private, protected, internal methods
+        /// <summary>
+        /// Called after the graphics device is created and before the game is about to start running.
+        /// </summary>
+        protected override void BeginRun()
+        {
+            base.BeginRun();
+
+            //start by showing the GarageGames splash screen
+            GuiSplashScreen splashScreen = new GuiSplashScreen(@"data\images\splashStudio");
+            GUICanvas.Instance.SetContentControl(splashScreen);
+        }
+        #endregion
+
+
+        #region Private, protected, internal fields
+        static Game _myGame;
+        #endregion
+    }
+}
