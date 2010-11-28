32dbe2015909105203cbadf9cabc41167a6b0bb2
consciousgamer <consciousgamer@d449e50b-a46f-350e-31ae-83ceb01ef3aa>
2010-11-28 21:54:19 +0000

    Framework components for Interactible objects and objects you can talk with

---
+++ FreeTheMarket/Game/Components/InteractibleComponent.cs
@@ -0,0 +1,99 @@
+//**btp_replace(namespace GarageGames.VCSTemplates.ItemTemplates,namespace FreeTheMarket.Components)
+//**btp_replace(class TestClass,public class InteractibleComponent)
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+using GarageGames.Torque.Core;
+using GarageGames.Torque.Util;
+using GarageGames.Torque.Sim;
+using GarageGames.Torque.T2D;
+using GarageGames.Torque.SceneGraph;
+using GarageGames.Torque.MathUtil;
+
+namespace FreeTheMarket.Components
+{
+    [TorqueXmlSchemaType]
+    public class InteractibleComponent : TorqueComponent, ITickObject
+    {
+        //======================================================
+        #region Static methods, fields, constructors
+        #endregion
+
+        //======================================================
+        #region Constructors
+        #endregion
+
+        //======================================================
+        #region Public properties, operators, constants, and enums
+
+        public T2DSceneObject SceneObject
+        {
+            get { return Owner as T2DSceneObject; }
+        }
+
+        #endregion
+
+        //======================================================
+        #region Public methods
+
+        public virtual void ProcessTick(Move move, float dt)
+        {
+            // todo: perform processing for component here
+        }
+
+        public virtual void InterpolateTick(float k)
+        {
+            // todo: interpolate between ticks as needed here
+        }
+
+        public override void CopyTo(TorqueComponent obj)
+        {
+            base.CopyTo(obj);
+        }
+
+        #endregion
+
+        //======================================================
+        #region Private, protected, internal methods
+
+        protected override bool _OnRegister(TorqueObject owner)
+        {
+            if (!base._OnRegister(owner) || !(owner is T2DSceneObject))
+                return false;
+
+            // todo: perform initialization for the component
+
+            // todo: look up interfaces exposed by other components
+            // E.g., 
+            // _theirInterface = Owner.Components.GetInterface<ValueInterface<float>>("float", "their interface name");            
+
+            return true;
+        }
+
+        protected override void _OnUnregister()
+        {
+            // todo: perform de-initialization for the component
+
+            base._OnUnregister();
+        }
+
+        protected override void _RegisterInterfaces(TorqueObject owner)
+        {
+            base._RegisterInterfaces(owner);
+
+            // todo: register interfaces to be accessed by other components
+            // E.g.,
+            // Owner.RegisterCachedInterface("float", "interface name", this, _ourInterface);
+        }
+
+
+        #endregion
+
+        //======================================================
+        #region Private, protected, internal fields
+        #endregion
+    }
+}
