00b957f374fdf36aee6517bf7517b86759959115
consciousgamer <consciousgamer@d449e50b-a46f-350e-31ae-83ceb01ef3aa>
2011-01-28 07:24:45 +0000

    Added FPS counter.

--- trunk/FreeTheMarket/Game/GUI/GuiPlay.cs
+++ trunk/FreeTheMarket/Game/GUI/GuiPlay.cs
@@ -17,8 +17,15 @@ namespace FreeTheMarket
 {
     public class GuiPlay : GUISceneview, IGUIScreen
     {        
-        DialogWindow _testDialogWindow;
+        DialogWindow _testDialogWindow;        
+        
+        // Used for camera schooling
         Vector2 _tempPosition;
+
+        // Used in FPS calculation and display
+        float _previousTime;      
+        GUITextStyle _FPSTextStyle;        
+        GUIText _FPSText;
 
         public GuiPlay()
         {
@@ -32,6 +39,25 @@ namespace FreeTheMarket
                 "Let's see how the dialog window handles this longer piece of text with different spacing.  It looks like this string has been broken up into multiple lines, but the question is how many?  Here is more text that should be put onto another screen.  Here's more text that will continue to roll onto another screen or two after it reaches the end of the window and rolls over.",
                 0.0f, 300.0f, 520.0f, 120.0f);
 
+            // FPS Stuff
+            _previousTime = Game.Instance.Engine.TorqueTime;                       
+
+            // Create the text style for all the text.
+            _FPSTextStyle = new GUITextStyle();
+            _FPSTextStyle.FontType = "Arial14"; // @"data\images\MyFont";
+            _FPSTextStyle.TextColor[0] = Color.White;
+            _FPSTextStyle.SizeToText = true;
+            _FPSTextStyle.Alignment = TextAlignment.JustifyLeft;
+            _FPSTextStyle.PreserveAspectRatio = true;
+
+            // Create all the GUI text lines.
+            _FPSText = new GUIText();
+            _FPSText.Style = _FPSTextStyle;
+            // The text needs to be inside the dialog window so the position should
+            // be offset from the dialog window's position.
+            _FPSText.Position = new Vector2(20.0f, 20.0f);
+            _FPSText.Visible = true;
+            _FPSText.Folder = this;
         }
 
 
@@ -39,6 +65,18 @@ namespace FreeTheMarket
         {            
             base.OnRender(offset, updateRect);
 
+            // Time in milliseconds
+            float time = Game.Instance.Engine.GameTime.ElapsedGameTime.Milliseconds;
+            if (time != 0.0)
+            {
+                // FPS in ms = (1/time)
+                // FPS in seconds = FPS in ms * 1000.0
+                float fps = 1000.0f/time;               
+
+                // Display FPS
+                _FPSText.Text = "FPS: "+fps.ToString();                
+            }
+            
             T2DSceneCamera camera = (T2DSceneCamera)this.Camera;
             T2DSceneObject player = (T2DSceneObject)TorqueObjectDatabase.Instance.FindObject("Player");
             if( camera != null && player != null )
